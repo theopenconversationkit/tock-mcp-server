0ae7ed1a93e6fc3c802c2da2ba6b5719ffb3ede2
Stephan Acquatella <stephan.acquatella@orange.com>
2026-06-05 18:02:36 +0200

    clean

--- config/config_test.go
+++ config/config_test.go
@@ -2,7 +2,7 @@ package config
 
 import "testing"
 
-func TestLoad_PreservesExtraHeaderKeys(t *testing.T) {
+func TestLoad_NormalizesExtraHeaderKeysWithViper(t *testing.T) {
 	cfg, err := Load("../config.yaml")
 	if err != nil {
 		t.Fatalf("Load() error: %v", err)
@@ -12,11 +12,11 @@ func TestLoad_PreservesExtraHeaderKeys(t *testing.T) {
 		t.Fatalf("ExtraHeaders is nil")
 	}
 
-	if _, ok := cfg.Tock.ExtraHeaders["X-Toki-Origin"]; !ok {
-		t.Fatalf("expected key X-Toki-Origin in ExtraHeaders, got: %#v", cfg.Tock.ExtraHeaders)
+	if _, ok := cfg.Tock.ExtraHeaders["x-toki-origin"]; !ok {
+		t.Fatalf("expected key x-toki-origin in ExtraHeaders, got: %#v", cfg.Tock.ExtraHeaders)
 	}
 
-	if _, ok := cfg.Tock.ExtraHeaders["X-Toki-Filter"]; !ok {
-		t.Fatalf("expected key X-Toki-Filter in ExtraHeaders, got: %#v", cfg.Tock.ExtraHeaders)
+	if _, ok := cfg.Tock.ExtraHeaders["x-toki-filter"]; !ok {
+		t.Fatalf("expected key x-toki-filter in ExtraHeaders, got: %#v", cfg.Tock.ExtraHeaders)
 	}
 }
